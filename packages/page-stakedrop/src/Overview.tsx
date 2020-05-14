5065979972092f5cb41f236f684460ca36717580
h4x3rotab <h4x3rotab@gmail.com>
2020-05-14 12:37:27 +0800

    Show realtime stats from api

--- packages/page-stakedrop/src/Overview.tsx
+++ packages/page-stakedrop/src/Overview.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 
 import { Balance } from '@polkadot/types/interfaces';
 import { BareProps } from '@polkadot/react-components/types';
 import { DeriveStakingOverview, DeriveSessionProgress } from '@polkadot/api-derive/types';
 import { useApi, useCall } from '@polkadot/react-hooks';
+import { formatBalance, formatNumber } from '@polkadot/util';
 
 import { SummaryBox, CardSummary } from '@polkadot/react-components';
 
@@ -29,7 +30,7 @@ function Overview ({next, setNominators, stakingOverview, whitelist}: Props): Re
   const { totalStaked } = useSortedTargets();
   const sessionInfo = useCall<DeriveSessionProgress>(api.derive.session?.progress, []);
   const totalIssuance = useCall<Balance>(api.query.balances?.totalIssuance, []);
-  const [percentage, setPercentage] = useState<string | undefined>();
+  const [percentage, setPercentage] = useState<string>('-');
 
   useEffect((): void => {
     totalIssuance && totalStaked?.gtn(0) && setPercentage(
@@ -38,38 +39,53 @@ function Overview ({next, setNominators, stakingOverview, whitelist}: Props): Re
   }, [totalIssuance, totalStaked]);
 
   const [stakeAmount, setStakeAmount] = useState<number>(0);
+  const [stakePoints, setStakePoints] = useState<number>(0);
+  const [stakePointsEst, setStakePointsEst] = useState<number>(0);
   useEffect(() => {
     if (!sessionInfo || !sessionInfo.activeEra) {
       return;
     }
-    if (new Date() < StakedropApi.startDate) {
-      return;
-    }
-    // console.log('era', sessionInfo.activeEra.toNumber(), sessionInfo.currentEra);
     const currentEra = sessionInfo.activeEra.toNumber();
-
-    async function sendRequest() {
-      const r = await StakedropApi.getTotalStaking(currentEra);
-      if (r.status == 'ok') {
-        const amount = r.result[0].total_amount;
-        setStakeAmount(amount);
+    [
+      async () => {
+        const r = await StakedropApi.getTotalStaking(currentEra);
+        if (r.status == 'ok') {
+          const amount = r.result[0].total_amount / 1000;
+          setStakeAmount(amount);
+          setStakePointsEst(StakedropApi.points(amount, 90));
+        }
+      },
+      async () => {
+        const r = await StakedropApi.getPoints(currentEra);
+        if (r.status == 'ok') {
+          const points = r.result[0].total_point_est;
+          setStakePoints(points);
+        }
       }
+    ].map(f => f());
+  }, [sessionInfo]);
+  const totalPHA = useMemo(() => {
+    let rate = stakePoints / StakedropApi.pointThreshold;
+    if (rate <= 1) {
+      rate = 1;
     }
-    sendRequest();
-
-  }, [sessionInfo]) 
+    return 27000000 * rate;
+  }, [stakePoints])
 
   return (
     <>
       <SummaryBox>
-        <CardSummary label={t('KSM total staked')}>
+        <CardSummary label={t('KSM network staked')}>
           {percentage}
         </CardSummary>
-        <CardSummary label={t('stakedrop participate rate')}>
-          {(stakeAmount / StakedropApi.pointThreshold * 100).toFixed(2)}%
+        <CardSummary label={t('KSM participating')}>
+          {formatBalance(stakeAmount, undefined, 0)}
+        </CardSummary>
+        <CardSummary label={t('stakedrop participate rate (est.)')}>
+          {(stakePointsEst / StakedropApi.pointThreshold * 100).toFixed(2)}%
         </CardSummary>
         <CardSummary label={t('PHA total reward (est.)')}>
-          27,000,000
+          {formatNumber(totalPHA)}
         </CardSummary>
       </SummaryBox>
 
